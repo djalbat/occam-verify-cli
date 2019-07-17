8b6fe62a31fd1f80656ebde1a9adf80f5aeabc56
James Smith <james.smith@openmathematics.org>
2019-07-17 21:04:56 +0100

    Re-organised verifying constructors.

--- bin/verify/declaration/constructor.js
+++ bin/verify/declaration/constructor.js
@@ -1,30 +1,15 @@
 'use strict';
 
-const parsers = require('occam-parsers');
-
-const Error = require('../../error'),
-      queries = require('../../queries'),
+const queries = require('../../queries'),
       Constructor = require('../../constructor'),
-      Configuration = require('../../configuration'),
-      ruleUtilities = require('../../utilities/rule'),
-      verifyTypeName = require('../../verify/typeName');
+      verifyTypeName = require('../../verify/typeName'),
+      verifyTermAsConstructor = require('../../verify/termAsConstructor');
 
-const { partTypes } = parsers,
-      { findRuleByName } = ruleUtilities,
-      { RuleNamePartType,
-        OptionalPartPartType,
-        GroupOfPartsPartType,
-        ChoiceOfPartsPartType,
-        OneOrMorePartsPartType,
-        ZeroOrMorePartsPartType } = partTypes,
-      { termNodeQuery, typeNameNodeQuery } = queries;
+const { termNodeQuery, typeNameNodeQuery } = queries;
 
 function verifyConstructorDeclaration(constructorDeclarationNode, context, rules) {
   const termNode = termNodeQuery(constructorDeclarationNode),
-        termRule = findRuleByName('term', rules),
-        rule = termRule,  ///
-        node = termNode,  ///
-        verified = verifyWithRule(rule, node, context, rules);
+        verified = verifyTermAsConstructor(termNode, context, rules);
 
   if (verified) {
     const typeNameNode = typeNameNodeQuery(constructorDeclarationNode),
@@ -37,149 +22,3 @@ function verifyConstructorDeclaration(constructorDeclarationNode, context, rules
 }
 
 module.exports = verifyConstructorDeclaration;
-
-function verifyWithNameRule(nameRule, node, context, rules) {
-  let verified = true;  ///
-
-  const childNodes = node.getChildNodes().slice(),  ///
-        childNode = childNodes.shift(),
-        terminalNode = childNode, ///
-        terminalNodeContent = terminalNode.getContent(),
-        name =terminalNodeContent,  ///
-        typeOrVariablePresent = context.isTypeOrVariablePresentByName(name);
-
-  if (!typeOrVariablePresent) {
-    const message = `There is no type or variable with the name '${name}' present.`;
-
-    throw new Error(node, message);
-  }
-
-  return verified;
-}
-
-function verifyWithRule(rule, node, context, rules) {
-  const definitions = rule.getDefinitions(),
-        verified = definitions.some((definition) => verifyWithDefinition(node, definition, context, rules));
-
-  return verified;
-}
-
-function verifyWithDefinition(node, definition, context, rules) {
-  const parts = definition.getParts(),
-        childNodes = node.getChildNodes().slice(),  ///
-        verified = verifyWithParts(childNodes, parts, context, rules);
-
-  return verified;
-}
-
-function verifyWithParts(childNodes, parts, context, rules) {
-  const verified = parts.every((part) => verifyWithPart(childNodes, part, context, rules));
-
-  return verified;
-}
-
-function verifyWithPart(childNodes, part, context, rules) {
-  let verified;
-
-  const partTerminalPart = part.isTerminalPart();
-
-  if (partTerminalPart) {
-    const terminalPart = part;  ///
-
-    verified = verifyWithTerminalPart(childNodes, terminalPart, context, rules);
-  } else {
-    const nonTerminalPart = part; ///
-
-    verified = verifyWithNonTerminalPart(childNodes, nonTerminalPart, context, rules);
-  }
-
-  return verified;
-}
-
-function verifyWithTerminalPart(childNodes, terminalPart, context, rules) {
-  let verified = false;
-
-  const childNode = childNodes.shift(),
-        childNodeTerminalNode = childNode.isTerminalNode();
-
-  if (childNodeTerminalNode) {
-    let terminalNode = childNode; ///
-
-    const significantToken = terminalNode.getSignificantToken(),
-          configuration = Configuration.fromSignificantToken(significantToken);
-
-    terminalNode = terminalPart.parse(configuration);
-
-    verified = (terminalNode !== undefined);
-  }
-
-  return verified;
-}
-
-function verifyWithNonTerminalPart(childNodes, nonTerminalPart, context, rules) {
-  let verified = false;
-
-  const nonTerminalPartType = nonTerminalPart.getType();
-
-  switch (nonTerminalPartType) {
-    case RuleNamePartType:
-      const ruleNamePart = nonTerminalPart;  ///
-
-      verified = verifyWithRuleNamePart(childNodes, ruleNamePart, context, rules);
-      break;
-
-    case OptionalPartPartType:
-      const optionalPart = nonTerminalPart; ///
-
-      verified = verifyWithOptionalPart(childNodes, optionalPart, context, rules);
-      break;
-
-    default:
-
-      debugger
-  }
-
-  return verified;
-}
-
-function verifyWithRuleNamePart(childNodes, ruleNamePart, context, rules) {
-  let verified = false;
-
-  const childNode = childNodes.shift();
-
-  if (childNode !== undefined) {
-    const childNodeNonTerminalNode = childNode.isNonTerminalNode();
-
-    if (childNodeNonTerminalNode) {
-      const nonTerminalNode = childNode,  ///
-            ruleNamePartRuleName = ruleNamePart.getRuleName(),
-            nonTerminalNodeRuleName = nonTerminalNode.getRuleName();
-
-      if (ruleNamePartRuleName === nonTerminalNodeRuleName) {
-        const node = nonTerminalNode, ///
-              name = ruleNamePartRuleName,  ///
-              rule = findRuleByName(name, rules);
-
-        if (name === 'name') {
-          const nameRule = rule,  ///
-
-          verified = verifyWithNameRule(nameRule, node, context, rules);
-        } else {
-          verified = verifyWithRule(rule, node, context, rules);
-        }
-      }
-    }
-  }
-
-  return verified;
-}
-
-function verifyWithOptionalPart(childNodes, optionalPart, context, rules) {
-  let verified = true;  ///
-
-  const part = optionalPart.getPart();
-
-  verifyWithPart(childNodes, part, context, rules);
-
-  return verified;
-}
--- bin/verify/term.js
+++ bin/verify/term.js
@@ -1,130 +1,125 @@
 'use strict';
 
-const Error = require('../error'),
-      queries = require('../queries'),
-      ruleUtilities = require('../utilities/rule');
-
-const { findRuleByName } = ruleUtilities,
-      { nameNodeQuery, termNodesQuery, parenthesisedTermsNodeQuery } = queries;
-
 function verifyTerm(termNode, context, rules) {
-  let term = undefined;
-
-  const termRule = findRuleByName('term', rules),
-        rule = termRule;  ///
-
-  verifyTermByRule(termNode, rule, context, rules);
-
   debugger
-
-  // const nameNode = nameNodeQuery(termNode),
-  //       nameNodeContent = nameNode.getContent(),
-  //       name = nameNodeContent, ///
-  //       parenthesisedTermsNode = parenthesisedTermsNodeQuery(termNode);
-  //
-  // if (parenthesisedTermsNode === undefined) {
-  //   const variable = context.retrieveVariableByName(name);
-  //
-  //   if (variable !== undefined) {
-  //     term = variable;  ///
-  //   } else {
-  //     const typeNames = undefined,
-  //           constructor = context.retrieveConstructorByNameAndTypeNames(name, typeNames),
-  //           constructorPresent = (constructor !== undefined);
-  //
-  //     if (constructorPresent) {
-  //       const typeName = constructor.getTypeName(),
-  //             compoundTerm = CompoundTerm.fromNameAndTypeName(name, typeName);
-  //
-  //       term = compoundTerm;  ///
-  //     }
-  //   }
-  // } else {
-  //   const termNodes = termNodesQuery(parenthesisedTermsNode),
-  //         terms = termNodes.map((termNode) => verifyTerm(termNode, context, rules)),
-  //         typeNames = terms.map((term) => term.getTypeName()),
-  //         constructor = context.retrieveConstructorByNameAndTypeNames(name, typeNames),
-  //         constructorPresent = (constructor !== undefined);
-  //
-  //   if (constructorPresent) {
-  //     const typeName = constructor.getTypeName(),
-  //           compoundTerm = CompoundTerm.fromNameTermsAndTypeName(name, terms, typeName);
-  //
-  //     term = compoundTerm;  ///
-  //   }
-  // }
-  //
-  // if (term === undefined) {
-  //   const node = termNode, ///
-  //         message = `The term '${name}' cannot be verified.`;
-  //
-  //   throw new Error(node, message);
-  // }
-
-  return term;
 }
 
 module.exports = verifyTerm;
 
-function verifyTermByRule(termNode, rule, context, rules) {
-  let term = undefined;
+// let term = undefined;
+//
+// const termRule = findRuleByName('term', rules),
+//     rule = termRule;  ///
+//
+// verifyTermByRule(termNode, rule, context, rules);
+//
+// debugger
+//
+// return term;
 
-  const definitions = rule.getDefinitions();
+// const nameNode = nameNodeQuery(termNode),
+//       nameNodeContent = nameNode.getContent(),
+//       name = nameNodeContent, ///
+//       parenthesisedTermsNode = parenthesisedTermsNodeQuery(termNode);
+//
+// if (parenthesisedTermsNode === undefined) {
+//   const variable = context.retrieveVariableByName(name);
+//
+//   if (variable !== undefined) {
+//     term = variable;  ///
+//   } else {
+//     const typeNames = undefined,
+//           constructor = context.retrieveConstructorByNameAndTypeNames(name, typeNames),
+//           constructorPresent = (constructor !== undefined);
+//
+//     if (constructorPresent) {
+//       const typeName = constructor.getTypeName(),
+//             compoundTerm = CompoundTerm.fromNameAndTypeName(name, typeName);
+//
+//       term = compoundTerm;  ///
+//     }
+//   }
+// } else {
+//   const termNodes = termNodesQuery(parenthesisedTermsNode),
+//         terms = termNodes.map((termNode) => verifyTerm(termNode, context, rules)),
+//         typeNames = terms.map((term) => term.getTypeName()),
+//         constructor = context.retrieveConstructorByNameAndTypeNames(name, typeNames),
+//         constructorPresent = (constructor !== undefined);
+//
+//   if (constructorPresent) {
+//     const typeName = constructor.getTypeName(),
+//           compoundTerm = CompoundTerm.fromNameTermsAndTypeName(name, terms, typeName);
+//
+//     term = compoundTerm;  ///
+//   }
+// }
+//
+// if (term === undefined) {
+//   const node = termNode, ///
+//         message = `The term '${name}' cannot be verified.`;
+//
+//   throw new Error(node, message);
+// }
 
-  definitions.some((definition) => {
-    term = verifyTermByDefinition(termNode, definition, context ,rules);
-
-    if (term !== undefined) {
-      return true;
-    }
-  });
-
-  return term;
-}
-
-function verifyTermByDefinition(termNode, definition, context, rules) {
-  let term = undefined;
-
-  const parts = definition.getParts(),
-        childNodes = termNode.getChildNodes().slice();  ///
-
-  parts.every((part) => {
-    verifyTermByPart(termNode, part, context, rules);
-  });
-
-  return term;
-}
-
-function verifyTermByPart(termNode, part, context, rules) {
-  let term = undefined;
-
-  const partType = part.getType();
-
-  switch (partType) {
-    case RuleNamePartType :
-      const ruleNamePart = part;  ///
-
-      term = verifyTermByRuleNamePart(termNode, ruleNamePart, context, rules);
-
-      break;
-  }
-
-  return term;
-}
-
-function verifyTermByRuleNamePart(termNode, ruleNamePart, context, rules) {
-  const ruleName = ruleNamePart.getRuleName(),
-        name = ruleName,  ///
-        rule = findRuleByName(name, rules);
-
-  if (rule === undefined) {
-    const node = termNode, ///
-          message = `The term '${name}' cannot be verified because the '${ruleName}' is missing.`;
-
-    throw new Error(node, message);
-  }
-
-  const term = verifyTermByRule(termNode, ruleNamePart, context, rules);
-
-  return term;
-}
+// function verifyTermByRule(termNode, rule, context, rules) {
+//   let term = undefined;
+//
+//   const definitions = rule.getDefinitions();
+//
+//   definitions.some((definition) => {
+//     term = verifyTermByDefinition(termNode, definition, context ,rules);
+//
+//     if (term !== undefined) {
+//       return true;
+//     }
+//   });
+//
+//   return term;
+// }
+//
+// function verifyTermByDefinition(termNode, definition, context, rules) {
+//   let term = undefined;
+//
+//   const parts = definition.getParts(),
+//         childNodes = termNode.getChildNodes().slice();  ///
+//
+//   parts.every((part) => {
+//     verifyTermByPart(termNode, part, context, rules);
+//   });
+//
+//   return term;
+// }
+//
+// function verifyTermByPart(termNode, part, context, rules) {
+//   let term = undefined;
+//
+//   const partType = part.getType();
+//
+//   switch (partType) {
+//     case RuleNamePartType :
+//       const ruleNamePart = part;  ///
+//
+//       term = verifyTermByRuleNamePart(termNode, ruleNamePart, context, rules);
+//
+//       break;
+//   }
+//
+//   return term;
+// }
+//
+// function verifyTermByRuleNamePart(termNode, ruleNamePart, context, rules) {
+//   const ruleName = ruleNamePart.getRuleName(),
+//         name = ruleName,  ///
+//         rule = findRuleByName(name, rules);
+//
+//   if (rule === undefined) {
+//     const node = termNode, ///
+//           message = `The term '${name}' cannot be verified because the '${ruleName}' is missing.`;
+//
+//     throw new Error(node, message);
+//   }
+//
+//   const term = verifyTermByRule(termNode, ruleNamePart, context, rules);
+//
+//   return term;
+// }
