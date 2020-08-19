d5e4b3683ba147a93826dd7ef67434e3cf568fc9
James Smith <james.smith@openmathematics.org>
2020-08-19 15:48:42 +0100

    Propagated.

--- bin/verify/indicativeConditional.js
+++ bin/verify/indicativeConditional.js
@@ -1,17 +1,36 @@
 "use strict";
 
-const queries = require("../miscellaneous/queries"),
+const Error = require("../error"),
+      queries = require("../miscellaneous/queries"),
+      nodeUtilities = require("../utilities/node"),
       verifyQualifiedStatement = require("../verify/qualifiedStatement"),
       verifyUnqualifiedStatement = require("../verify/unqualifiedStatement");
 
-const { qualifiedStatementNodeQuery, unqualifiedStatementNodeQuery } = queries;
+const { nodeAsString } = nodeUtilities,
+      { qualifiedStatementNodeQuery, unqualifiedStatementNodeQuery } = queries;
 
 function verifyIndicativeConditional(indicativeConditionalNode, fileContext) {
   const unqualifiedStatementNode = unqualifiedStatementNodeQuery(indicativeConditionalNode);
 
+  if (unqualifiedStatementNode === undefined) {
+    const node = indicativeConditionalNode,  ///
+          indicativeConditionalString = nodeAsString(indicativeConditionalNode),
+          message = `The indicative conditional '${indicativeConditionalString}' cannot be verified because its antecedent is not an unqualified statement.`;
+
+    throw new Error(node, message);
+  }
+
   verifyUnqualifiedStatement(unqualifiedStatementNode, fileContext);
 
   const qualifiedStatementNode = qualifiedStatementNodeQuery(indicativeConditionalNode);
+
+  if (qualifiedStatementNode === undefined) {
+    const node = indicativeConditionalNode,  ///
+          indicativeConditionalString = nodeAsString(indicativeConditionalNode),
+          message = `The indicative conditional '${indicativeConditionalString}' cannot be verified because its consequent is not a qualified statement.`;
+
+    throw new Error(node, message);
+  }
 
   verifyQualifiedStatement(qualifiedStatementNode, fileContext);
 }
