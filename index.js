a2994ea2fe00595aa720e11cb03b96ccdd605d77
James Smith <james.smith@djalbat.com>
2022-09-24 12:00:57 +0100

    Created an index file.

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const verifyPackage = require("./bin/verify/package"),
+      PackageContext = require("./bin/context/package");
+
+module.exports = {
+  verifyPackage,
+  PackageContext
+};
