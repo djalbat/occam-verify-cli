f8dffe431b763a22e2b84bf8615befcf303b42d0
James Smith <james.smith@djalbat.com>
2022-09-27 18:40:04 +0100

    Propagated.

--- bin/verify/metaproof.js
+++ bin/verify/metaproof.js
@@ -1,5 +1,19 @@
 "use strict";
 
+const { loggingUtilities } = require("necessary");
+
+const Rule = require("../rule"),
+    verifyLabels = require("../verify/labels"),
+    verifyMetaproof = require("../verify/metaproof"),
+    verifyUnqualifiedMetastatement = require("../verify/unqualifiedMetastatement");
+
+const { nodeQuery, nodesQuery } = require("../utilities/query"),
+    { labelsAsString, nodesAsString } = require("../utilities/string");
+
+const { log } = loggingUtilities;
+
+const metaproofDerivationQuery = nodeQuery("/metaproof/metaproof!");
+
 function verifyMetaproof(metaproofNode, premiseMetastatementNodes, conclusionMetastatementNode, context) {
   let metaproofVerified = false;
 
