457779cf24b1e09e968c9e478fe58f52277894bf
James Smith <james.smith@djalbat.com>
2024-10-13 10:38:52 +0100

    Propagated.

--- src/utilities/array.js
+++ src/utilities/array.js
@@ -2,37 +2,7 @@
 
 import { arrayUtilities } from "necessary";
 
-export const { first, last, front, tail, push, find, match, clear, prune, extract, filter, compress, separate } = arrayUtilities;
-
-export function reverse(array) {
-  array = [ ///
-    ...array
-  ].reverse();
-
-  return array;
-}
-
-export function correlate(arrayA, arrayB, callback) {
-  arrayB = [  ///
-    ...arrayB
-  ];
-
-  const correlates = arrayA.every((elementA) => {
-    const elementB = extract(arrayB, (elementB) => {
-      const result = callback(elementA, elementB);
-
-      if (result) {
-        return true;
-      }
-    }) || null;
-
-    if (elementB !== null) {
-      return true;
-    }
-  });
-
-  return correlates;
-}
+export const { first, last, front, tail, push, find, match, clear, prune, reverse, extract, correlate, filter, compress, separate } = arrayUtilities;
 
 export function leftDifference(arrayA, arrayB) {
   filter(arrayA, (elementA) => {
