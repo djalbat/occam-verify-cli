53e7a31647027a0260e709fc5765d4546da991a3
James Smith <james.smith@djalbat.com>
2025-07-18 19:02:16 +0100

    Imported the default custom grammar type pattern for the base object type's name.

--- src/typeNames.js
+++ src/typeNames.js
@@ -1,3 +1,7 @@
 "use strict";
 
-export const OBJECT_TYPE_NAME = "Object";
+import { defaultCustomGrammar } from "occam-custom-grammars";
+
+const typePattern = defaultCustomGrammar.getTypePattern();
+
+export const OBJECT_TYPE_NAME = typePattern;  ///
