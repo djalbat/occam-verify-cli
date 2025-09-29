9ec6d3894392f9752fe2725e2324f3f8c761f9ae
James Smith <james.smith@djalbat.com>
2025-09-29 19:48:16 +0100

    Small changes to the metavariable node.

--- src/node/metavariable.js
+++ src/node/metavariable.js
@@ -2,38 +2,21 @@
 
 import NonTerminalNode from "../node/nonTerminal";
 
-import { ARGUMENT_RULE_NAME } from "../ruleNames";
+import { TERM_RULE_NAME, TYPE_RULE_NAME } from "../ruleNames";
 
 export default class MetavariableNode extends NonTerminalNode {
   getTermNode() {
-    let termNode = null;
-
-    const argumentNode = this.getArgumentNode();
-
-    if (argumentNode !== null) {
-      termNode = argumentNode.getTermNode();
-    }
+    const ruleName = TERM_RULE_NAME,
+          termNode = this.getNodeByRuleName(ruleName);
 
     return termNode;
   }
 
   getTypeNode() {
-    let typeNode = null;
-
-    const argumentNode = this.getArgumentNode();
-
-    if (argumentNode !== null) {
-      typeNode = argumentNode.getTypeNode();
-    }
+    const ruleName = TYPE_RULE_NAME,
+          typeNode = this.getNodeByRuleName(ruleName);
 
     return typeNode;
-  }
-
-  getArgumentNode() {
-    const ruleName = ARGUMENT_RULE_NAME,
-          argumentNode = this.getNodeByRuleName(ruleName);
-
-    return argumentNode;
   }
 
   getMetavariableName() {
