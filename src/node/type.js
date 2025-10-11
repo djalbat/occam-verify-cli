9ecb2473811e15df4b305458e12039062270c19e
James Smith <james.smith@djalbat.com>
2025-10-11 15:01:11 +0100

    More progress.

--- src/node/type.js
+++ src/node/type.js
@@ -3,11 +3,21 @@
 import NonTerminalNode from "../node/nonTerminal";
 
 export default class TypeNode extends NonTerminalNode {
+  isPrefixed() {
+    const multiplicity = this.getMultiplicity(),
+          prefixed = (multiplicity > 1);
+
+    return prefixed;
+  }
+
   getTypeName() {
     let typeName;
 
+    const prefixed = this.isPrefixed(),
+          nameIndex = prefixed ? 2 : 0;
+
     this.someChildNode((childNode, index) => {
-      if (index === 0) {
+      if (index === nameIndex) {
         const typeTerminalNode = childNode, ///
               content = typeTerminalNode.getContent();
 
@@ -23,16 +33,22 @@ export default class TypeNode extends NonTerminalNode {
   getTypePrefixName() {
     let typePrefixName = null;
 
-    this.someChildNode((childNode, index) => {
-      if (index === 2) {
-        const typeTerminalNode = childNode, ///
-              content = typeTerminalNode.getContent();
+    const prefixed = this.isPrefixed();
 
-        typePrefixName = content; ///
+    if (prefixed) {
+      const prefixIndex = 0;
 
-        return true;
-      }
-    });
+      this.someChildNode((childNode, index) => {
+        if (index === prefixIndex) {
+          const typeTerminalNode = childNode, ///
+                content = typeTerminalNode.getContent();
+
+          typePrefixName = content; ///
+
+          return true;
+        }
+      });
+    }
 
     return typePrefixName;
   }
