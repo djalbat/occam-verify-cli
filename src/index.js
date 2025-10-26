bed505d7454dac1d948ef3540d810a2e25954229
James Smith <james.smith@djalbat.com>
2025-10-26 18:37:58 +0000

    Added file contexts to the index file.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 import "./preamble";
 
 export { default as Log } from "./log";
+export { default as FileContext } from "./context/file";
 export { default as ReleaseContext } from "./context/release";
 export { default as releaseUtilities } from "./utilities/release";
 export { default as customGrammarUtilities } from "./utilities/customGrammar";
