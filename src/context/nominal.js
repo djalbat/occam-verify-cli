e6554cf6d2b4ae405866ab7d85b68ed10395702b
James Smith <james.smith@djalbat.com>
2026-02-10 13:33:10 +0000

    Fixes around the contexts.

--- src/context/nominal.js
+++ src/context/nominal.js
@@ -1,31 +1,9 @@
 "use strict";
 
 import { Context } from "occam-languages";
-import { nominalLexer, nominalParser } from "../utilities/nominal";
 
 class NominalContext extends Context {
-  constructor(context, lexer, parser) {
-    super(context);
-
-    this.lexer = lexer;
-    this.parser = parser;
-  }
-
-  getLexer() {
-    return this.lexer;
-  }
-
-  getParser() {
-    return this.parser;
-  }
-
-  static fromNothing() {
-    const lexer = nominalLexer, ///
-          parser = nominalParser, ///
-          nominalContext = new NominalContext(lexer, parser);
-
-    return nominalContext;
-  }
+  static fromNothing() { return Context.fromNothing(NominalContext); }
 }
 
 const nominalContext = NominalContext.fromNothing();
